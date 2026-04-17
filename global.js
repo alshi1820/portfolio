fa2c5a6e69ca48929aaa9db65bde89837cfd0e83
Allie Shi <alshi@ucsd.edu>
2026-04-17 12:45:17 -0700

    js

--- global.js
+++ global.js
@@ -30,12 +30,13 @@ for (let p of pages) {
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
-    nav.append(a);
-    
+    if (a.host === location.host && a.pathname === location.pathname) {
+        a.classList.add('current');
+    }
     a.classList.toggle(
         'current',
         a.host === location.host && a.pathname === location.pathname,
     );
-    
+    nav.append(a);
 }
 
