acb1cdea59ca9facf8079db06f3c23550d702771
Allie Shi <alshi@ucsd.edu>
2026-04-17 10:38:54 -0700

    js

--- global.js
+++ global.js
@@ -20,21 +20,23 @@ let nav = document.createElement('nav');
 document.body.prepend(nav);
 for (let p of pages) {
     let url = p.url;
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
     let title = p.title;
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
-    nav.append(a);
+    
     const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
     ? "/"                  // Local server
     : "/website/";         // GitHub Pages repo name
-    url = !url.startsWith('http') ? BASE_PATH + url : url;
+    
     if (a.host === location.host && a.pathname === location.pathname) {
-    a.classList.add('current');
+        a.classList.add('current');
     }
     a.classList.toggle(
-    'current',
-    a.host === location.host && a.pathname === location.pathname,
+        'current',
+        a.host === location.host && a.pathname === location.pathname,
     );
+    nav.append(a);
 }
 
