42e1d3839469faec725ff13561ba7e6ae179568b
Allie Shi <alshi@ucsd.edu>
2026-04-17 16:22:57 -0700

    js

--- global.js
+++ global.js
@@ -42,14 +42,15 @@ for (let p of pages) {
     nav.append(a);
     
 }
-
+const prefersDark = matchMedia("(prefers-color-scheme: dark)").matches;
+const autoLabel = prefersDark ? "Automatic (Dark)" : "Automatic (Light)";
 document.body.insertAdjacentHTML(
   'afterbegin',
   `
 	<label class="color-scheme">
 		Theme:
 		<select>
-			<option value="light dark">Automatic</option>
+			<option value="light dark">${autoLabel}</option>
             <option value="light">Light</option>
             <option value="dark">Dark</option>
 		</select>
