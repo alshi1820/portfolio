f4eced931de77790a2def4c7d1f4f5b132148207
Allie Shi <alshi@ucsd.edu>
2026-04-17 10:25:42 -0700

    js

--- global.js
+++ global.js
@@ -19,7 +19,12 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 for (let p of pages) {
-  let url = p.url;
-  let title = p.title;
-  nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    let url = p.url;
+    let title = p.title;
+    nav.insertAdjacentHTML('beforeend', `<a href="${url}">${title}</a>`);
+    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+    ? "/"                  // Local server
+    : "/website/";         // GitHub Pages repo name
+    url = !url.startsWith('http') ? BASE_PATH + url : url;
 }
+
