b7ef1dd55e45e8048bf0e1c5d4f727e2fd6ccc5a
Allie Shi <alshi@ucsd.edu>
2026-04-17 04:55:15 -0700

    js

--- global.js
+++ global.js
@@ -3,3 +3,11 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+let currentLink = navLinks.find(
+  (a) => a.host === location.host && a.pathname === location.pathname,
+);
+if (currentLink) {
+  // or if (currentLink !== undefined)
+  currentLink?.classList.add('current');
+}
+
