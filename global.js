14545a08f95248852a65910473476b64f6a83813
Allie Shi <alshi@ucsd.edu>
2026-04-17 11:00:03 -0700

    js

--- global.js
+++ global.js
@@ -3,6 +3,9 @@ console.log('IT’S ALIVE!');
 function $$(selector, context = document) {
   return Array.from(context.querySelectorAll(selector));
 }
+const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
+        ? "/"                  // Local server
+        : "/website/";  
 let currentLink = navLinks.find(
   (a) => a.host === location.host && a.pathname === location.pathname,
 );
@@ -20,16 +23,14 @@ let pages = [
 let nav = document.createElement('nav');
 document.body.prepend(nav);
 for (let p of pages) {
-    let url = p.url;
+    let url = p.url; 
     url = !url.startsWith('http') ? BASE_PATH + url : url;
     let title = p.title;
     let a = document.createElement('a');
     a.href = url;
     a.textContent = title;
     
-    const BASE_PATH = (location.hostname === "localhost" || location.hostname === "127.0.0.1")
-    ? "/"                  // Local server
-    : "/website/";         // GitHub Pages repo name
+    
     a.classList.toggle(
         'current',
         a.host === location.host && a.pathname === location.pathname,
