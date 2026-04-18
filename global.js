116711c85d968d959844df0168b4b205e5ba0d95
Allie Shi <alshi@ucsd.edu>
2026-04-17 19:09:48 -0700

    j

--- global.js
+++ global.js
@@ -58,8 +58,8 @@ document.body.insertAdjacentHTML(
 
 const select = document.querySelector('color-scheme');
 select.addEventListener('input', function (event) {
-  console.log('color scheme changed to', event.target.value);
-  document.documentElement.style.setProperty('color-scheme', event.target.value);
+    console.log('color scheme changed to', event.target.value);
+    document.documentElement.style.setProperty('color-scheme', event.target.value);
 });
 
 
