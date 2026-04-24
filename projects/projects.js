aa2cc80ca6e9500b5d38a01eefd15e8f0b5bad45
Allie Shi <alshi@ucsd.edu>
2026-04-24 10:48:15 -0700

    p

--- projects/projects.js
+++ projects/projects.js
@@ -1,7 +1,13 @@
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
+const title = document.querySelector('.projects-title');
+
+if (title && Array.isArray(projects)) {
+  title.textContent = `(${projects.length}) Projects`;
+}
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
+
 // let projects = [];
 
 // try {
