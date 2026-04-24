a3eece12c18aa6df261b391904f488a549f009d7
Allie Shi <alshi@ucsd.edu>
2026-04-24 13:08:00 -0700

    p

--- index.js
+++ index.js
@@ -4,9 +4,9 @@ const latestProjects = projects.slice(0, 3);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(latestProjects, projectsContainer, 'h2');
 const githubData = await fetchGithubData('alshi1820');
-const dl = document.querySelector('#profile-stats');
-if (dl) {
-    dl.innerHTML = `
+const profileStats = document.querySelector('#profile-stats');
+if (profileStats) {
+    profileStats.innerHTML = `
         <dl>
           <dt>Public Repos:</dt><dd>${githubData.public_repos}</dd>
           <dt>Public Gists:</dt><dd>${githubData.public_gists}</dd>
