da6b926744c055aab6e62a952a0832d63681af59
Allie Shi <alshi@ucsd.edu>
2026-04-24 10:35:10 -0700

    p

--- global.js
+++ global.js
@@ -87,6 +87,32 @@ export async function fetchJSON(url) {
     console.error('Error fetching or parsing JSON data:', error);
   }
 }
+export function renderProjects(projects, containerElement, headingLevel = 'h2') {
+    if (!containerElement){
+        console.error('No valid container provided');
+        return;
+    }
+    containerElement.innerHTML = '';
+    if (!projects || projects.length === 0){
+        containerElement.innerHTML = '<p>No projects available yet.</p>';
+        return;
+    }
+    const validHeadings = ['h1','h2','h3','h4','h5','h6'];
+    if (!validHeadings.includes(headingLevel)){
+        console.warn('Inavlid headingLevel, defuaulting to h2');
+        headingLevel = 'h2';
+    }
+    for (const proj of projects){
+        const article = document.createElement('article');
+        article.innerHTML = `
+            <h${headingLevel}${proj.title ? proj.title : 'Untitled Project'}</${headingLevel}>
+            <img src="${proj.image ? proj.image : 'images/placeholder.png'}" alt="${proj.title ? proj.title : 'Project image'}">
+            <p>${proj.description ? proj.description : 'No description available.'}</p>
+        `;
+        containerElement.appendChild(article);
+    }
+  
+}
 
 
 
