a06e4d321a90fd442ea0729c67a46553e6046d62
Allie Shi <alshi@ucsd.edu>
2026-04-23 22:50:45 -0700

    json

--- global.js
+++ global.js
@@ -74,7 +74,17 @@ select.addEventListener('input', function (event) {
     localStorage.colorScheme = event.target.value;
 });
 
-
+export async function fetchJSON(url) {
+  try {
+    // Fetch the JSON file from the given URL
+    const response = await fetch(url);
+    if (!response.ok) {
+        throw new Error(`Failed to fetch projects: ${response.statusText}`);
+    }
+  } catch (error) {
+    console.error('Error fetching or parsing JSON data:', error);
+  }
+}
 
 
 
