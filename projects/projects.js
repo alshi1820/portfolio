4e5fb26a1cba53070faea00d53fea18b4704cc7c
Allie Shi <alshi@ucsd.edu>
2026-05-01 13:30:05 -0700

    p

--- projects/projects.js
+++ projects/projects.js
@@ -14,22 +14,11 @@ let arc = d3.arc().innerRadius(0).outerRadius(50)({
   endAngle: 2 * Math.PI,
 });
 
-
 let data = [1, 2];
-let total = 0;
-
-for (let d of data) {
-  total += d;
-}
-let angle = 0;
-let arcData = [];
-
-for (let d of data) {
-  let endAngle = angle + (d / total) * 2 * Math.PI;
-  arcData.push({ startAngle: angle, endAngle });
-  angle = endAngle;
-}
+let sliceGenerator = d3.pie();
+let arcData = sliceGenerator(data);
 let arcs = arcData.map((d) => arcGenerator(d));
+
 arcs.forEach((arc) => {
     d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
 });
