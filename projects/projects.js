53bf7ee86a6456d7710f4e9028bb3d94d5354467
Allie Shi <alshi@ucsd.edu>
2026-05-02 00:32:02 -0700

    js

--- projects/projects.js
+++ projects/projects.js
@@ -9,23 +9,18 @@ if (title && Array.isArray(projects)) {
 }
 
 import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
-let arc = d3.arc().innerRadius(0).outerRadius(50)({
-  startAngle: 0,
-  endAngle: 2 * Math.PI,
-});
-
+let arcGenerator = d3.arc()
+  .innerRadius(0)
+  .outerRadius(50);
 let data = [1, 2];
 let sliceGenerator = d3.pie();
 let arcData = sliceGenerator(data);
-let arcs = arcData.map((d) => arcGenerator(d));
-
-arcs.forEach((arc) => {
-    d3.select('svg').append('path').attr('d', arc).attr('fill', 'red');
-});
+let arcs = arcData.map(d => arcGenerator(d));
 let colors = ['gold', 'purple'];
+
 arcs.forEach((arc, idx) => {
-    d3.select('#projects-pie-plot')
-      .append('path')
-      .attr('d', arc)
-      .attr('fill',colors[idx]); // Fill in the attribute for fill color via indexing the colors variable
-})
+  d3.select('#projects-pie-plot')   
+    .append('path')
+    .attr('d', arc)
+    .attr('fill', colors[idx]);
+});
