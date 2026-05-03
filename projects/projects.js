3311cd7d544e125653a5b491efa6794081009182
Allie Shi <alshi@ucsd.edu>
2026-05-02 21:43:45 -0700

    Update projects.js

--- projects/projects.js
+++ projects/projects.js
@@ -1,5 +1,10 @@
 import { fetchJSON, renderProjects } from '../global.js';
 const projects = await fetchJSON('../lib/projects.json');
+let rolledData = d3.rollups(
+  projects,
+  (v) => v.length,
+  (d) => d.year
+);
 const projectsContainer = document.querySelector('.projects');
 renderProjects(projects, projectsContainer, 'h2');
 const title = document.querySelector('.projects-title');
@@ -12,14 +17,9 @@ import * as d3 from 'https://cdn.jsdelivr.net/npm/d3@7.9.0/+esm';
 let arcGenerator = d3.arc()
   .innerRadius(0)
   .outerRadius(50);
-let data = [
-  { value: 1, label: 'apples' },
-  { value: 2, label: 'oranges' },
-  { value: 3, label: 'mangos' },
-  { value: 4, label: 'pears' },
-  { value: 5, label: 'limes' },
-  { value: 5, label: 'cherries' },
-];
+let data = rolledData.map(([year, count]) => {
+  return { value: count, label: year };
+});
 let sliceGenerator = d3.pie().value((d) => d.value);
 let arcData = sliceGenerator(data);
 let arcs = arcData.map(d => arcGenerator(d));
